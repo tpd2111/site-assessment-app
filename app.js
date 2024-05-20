f651c5dd15ce10b8014a4f3d28d0bc4461b2dd45
tpd2111 <122829848+tpd2111@users.noreply.github.com>
2024-05-20 11:44:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Declare sssiLayer in the global scope
 var sssiLayer;
 
+// Add drawing functionality
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    },
+    draw: {
+        polyline: true,
+        polygon: true,
+        circle: false,
+        marker: false,
+        rectangle: true
+    }
+});
+
+// Initially disable the draw control
+map.removeControl(drawControl);
+
 // Load UK SSSI data from the GeoJSON file on S3
 fetch('https://tpd2111bucket.s3.eu-north-1.amazonaws.com/SSSI_Impact_Risk_Zones_England_-3698026961114884751.geojson')
     .then(response => {
@@ -36,26 +56,11 @@ fetch('https://tpd2111bucket.s3.eu-north-1.amazonaws.com/SSSI_Impact_Risk_Zones_
         };
 
         L.control.layers(baseLayers, overlays).addTo(map);
+
+        // Enable the draw control once the SSSI layer is loaded
+        map.addControl(drawControl);
     })
     .catch(error => console.error('Error loading GeoJSON:', error));
-
-// Add drawing functionality
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
-
-var drawControl = new L.Control.Draw({
-    edit: {
-        featureGroup: drawnItems
-    },
-    draw: {
-        polyline: true,
-        polygon: true,
-        circle: false,
-        marker: false,
-        rectangle: true
-    }
-});
-map.addControl(drawControl);
 
 map.on(L.Draw.Event.CREATED, function (event) {
     var layer = event.layer;
